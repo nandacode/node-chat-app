cdfc90824b7e6219254425b5decd7361f4cc2773
nandakumarp <nandakumar1990@gmail.com>
2018-09-09 13:32:15 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+var publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`App started listening on port ${port}`);
+});
